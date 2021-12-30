e789c47f931f08bb16d82bcf1c909f1582e94fce
Yuriy Goncharov <goncharov@comfortel.pro>
2021-12-30 14:21:26 +0300

    cisco_dial

--- cisco-dial/cisco_dial.sh
+++ cisco-dial/cisco_dial.sh
@@ -7,6 +7,10 @@ NUMBER=$(xclip -o |  sed -r 's/[\(\)\ -\+]//g')
 if [[ ! $NUMBER =~ [0-9]+ ]]; then
     exit 1
 fi
+
+if [[ ${#NUMBER} -eq 10 ]]; then
+    NUMBER=7$NUMBER
+fi
 curl -X POST   http://$PHONE_IP/CGI/Execute                  \
         -H 'Content-Type: application/x-www-form-urlencoded' \
         -d "XML=<CiscoIPPhoneExecute><ExecuteItem Priority=\"0\" URL=\"Key:$LINE\"/></CiscoIPPhoneExecute>"
