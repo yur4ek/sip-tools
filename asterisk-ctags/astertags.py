a665c39e447b115de5f0397b85970c3d801ac274
Yuriy Goncharov <goncharov@comfortel.pro>
2022-02-04 15:24:44 +0300

    Script to generate asterisk tags file for vim

---
+++ asterisk-ctags/astertags.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+
+CONF='/etc/asterisk'
+TAGFILE='/var/lib/vim/tags'
+
+def parse_conf(filename):
+    val = {}
+    for line in open(filename):
+        reg = re.match(r'\[([^\s]+)\]', line)
+        if reg:
+            tag = reg.groups()[0]
+            if tag.startswith('macro-'):
+
+            val[tag] = [tag, filename, '/^{0}$/;"'.format(line.strip()), 's']
+    return val
+
+
+tags = {}
+for dirpath, dirnames, filenames in os.walk(CONF):
+    for i in filenames:
+        if re.match(r'(sip|exten).*conf$', i):
+            tags.update(parse_conf(os.path.join(CONF,i)))
+
+outfile = open(TAGFILE, 'w')
+for k in sorted(tags.keys()):
+    outfile.write('\t'.join(tags[k]) + '\n')
+
+outfile.close()
