ef901742f7f25b698dfa63d57571c9d10b7128c0
Yuriy Goncharov <goncharov@comfortel.pro>
2022-02-04 17:29:27 +0300

    asterisk-ctags support for *.ael format

--- asterisk-ctags/astertags.py
+++ asterisk-ctags/astertags.py
@@ -19,12 +19,24 @@ def parse_conf(filename):
             val[tag] = [tag, filename, '/^{0}$/;"'.format(line.strip()), 's']
     return val
 
+def parse_ael(filename):
+    val = {}
+    for line in open(filename):
+        reg = re.match(r'(context|macro) ([^\s({]+)', line)
+        if reg:
+            tag = reg.groups()[1]
+            val[tag] = [tag, filename, '/^{0}$/;"'.format(line.strip()), 's']
+    return val
+
 
 tags = {}
 for dirpath, dirnames, filenames in os.walk(CONF):
     for i in filenames:
         if re.match(r'(sip|exten).*conf$', i):
             tags.update(parse_conf(os.path.join(CONF,i)))
+        elif i.endswith('.ael'):
+            tags.update(parse_ael(os.path.join(CONF,i)))
+
 
 outfile = open(TAGFILE, 'w')
 for k in sorted(tags.keys()):
